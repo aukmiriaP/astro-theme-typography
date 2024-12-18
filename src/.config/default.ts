e9bf7115b190cf7188fe6fda9cc0abcb1900a067
aukmiriaP <202422089034@mail.bnu.edu.cn>
2024-12-18 15:55:31 +0800

    删除《容忍与自由》

--- src/.config/default.ts
+++ src/.config/default.ts
@@ -47,7 +47,7 @@ export const defaultConfig: ThemeConfig = {
         href: '/about',
       },
     ],
-    categoryMap: [{ name: '胡适', path: 'hu-shi' }],
+    categoryMap: [{ name: '记录', path: 'record' }],
     footer: [
       '© %year <a target="_blank" href="%website">%author</a>',
       'Theme <a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">Typography</a> by <a target="_blank" href="https://moeyua.com">Moeyua</a>',
